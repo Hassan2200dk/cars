2e3755b2fb98407ba38539d525cb8a83ffaed869
Hassan2200dk <hassan.2002@outlook.dk>
2023-08-28 13:11:08 +0200

    opgave orm

--- src/main/java/dat3/car/cars/entity/Car.java
+++ src/main/java/dat3/car/cars/entity/Car.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Car {
+public class Car extends AdminDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
